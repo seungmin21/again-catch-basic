99de2d35c86bfbe7fe8ff5317b9dfd1d4716d7af
ohseungmin <ohmin2886@gmail.com>
2024-03-07 16:34:00 +0900

    2개 이상 매개변수를 사용한 함수와 데이터타입 검사를 진행하는 함수 두개를 사용한 함수 작성

--- function/function.py
+++ function/function.py
@@ -2,3 +2,34 @@ def greet(name):
     print("Hello,", name)
 
 greet("ohmin")
+
+def basic(x, y):
+    sum = x + y
+    return sum
+
+x = "Hello"
+y = " World!"
+result = basic(x, y)
+print(result)
+
+
+# 만약 매개변수가 문자열일 경우 맞으면 정답 틀리면 틀림
+
+def question(parameter):
+    if isinstance (parameter, str):
+        return "정답"
+    else:
+        return "틀림"
+
+string = "문자"
+print(question(string))
+
+def dataType(every):
+    if (type(every) == str):
+        return "정답"
+    else:
+        return "틀림"
+
+every = "문장"
+print(dataType(every))
+
