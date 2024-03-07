c989fed7f759766697d45a17c1221308b0f7e889
Oh seung min <ohmin@gmail.com>
2024-03-07 20:41:14 +0900

    다른 클래스에게 상속할 클래스를 부여해서 사용하는 방법

--- Class/Class.js
+++ Class/Class.js
@@ -3,9 +3,23 @@ class Dog {
     this.name = name;
     this.age = age;
   }
+  dogSound() {
+    console.log(this.name, "sound is barks");
+  }
 }
 
 let dog = new Dog("buddy", 4);
-console.log(dog) // Dog { name: 'buddy', age: 4 }
-console.log(dog.name) // buddy
+console.log(dog); // Dog { name: 'buddy', age: 4 }
+console.log(dog.name); // buddy
 
+class Cat extends Dog {
+  catSound() {
+    console.log(this.name, "sound is purr");
+  }
+}
+
+let cat = new Cat("Tom", 2);
+console.log(cat); // Cat { name: 'Tom', age: 2}
+console.log(cat.name); // Tom
+cat.catSound();
+cat.dogSound();
