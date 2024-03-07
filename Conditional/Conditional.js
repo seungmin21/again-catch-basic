e31a16a5ad8d143a089c89c36befc178fc9c0eea
ohseungmin <ohmin2886@gmail.com>
2024-03-07 14:54:22 +0900

    기본 조건문 작성 else if 사용 방식까지

---
+++ Conditional/Conditional.js
@@ -0,0 +1,27 @@
+const number = 19
+
+// if (number === "String") {
+    // console.log("정답")
+// } else {
+    // console.log("틀림")
+// }
+
+// if (number !== 19) {
+    // console.log("19가 아닙니다.")
+// } else {
+    // console.log("19가 맞습니다.")
+// }
+
+// if (number > 0.5) {
+    // console.log("크다")
+// } else {
+    // console.log("작다")
+// }
+
+if (number > 0) {
+    console.log("Positive number")
+} else if (number === 0) {
+    console.log("not same number")
+} else {
+    console.log("Negative number")
+}
