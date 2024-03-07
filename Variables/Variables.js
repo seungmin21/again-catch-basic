8d0f9879341438f4731cb489d1fffa952082dc80
ohseungmin <ohmin2886@gmail.com>
2024-03-07 14:39:47 +0900

    두개의 선언에서 선언된 기존 값을 바꾸는 방법

--- Variables/Variables.js
+++ Variables/Variables.js
@@ -11,6 +11,12 @@ name = "ohmin";
 age = 28;
 height = 171;
 
+let temp = age;
+
+age = height
+
+height = temp
+
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Height:", height);
