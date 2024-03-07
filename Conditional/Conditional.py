97e69d90aa812530945ac232beb03244b7ff048e
ohseungmin <ohmin2886@gmail.com>
2024-03-07 14:59:29 +0900

    python 조건문 작성

---
+++ Conditional/Conditional.py
@@ -0,0 +1,16 @@
+number = 10
+if (number > 0):
+    print("number 0보다 큽니다.")
+else:
+    print("number 0보다 작습니다.")
+
+
+if (number != 0):
+    print("number는 0이 아닙니다.")
+else:
+    print("number는 0입니다.")
+
+if (number % 2):
+    print("number는 짝수입니다.")
+else:
+    print("number는 홀수입니다.")
